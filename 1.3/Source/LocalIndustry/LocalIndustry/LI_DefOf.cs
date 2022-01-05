b9f6cf07391ced96059e6cec94750063581debc3
Taranchuk <nfhfyxer@gmail.com>
2022-01-05 09:28:12 +0200

    added PRF patch

---
+++ 1.3/Source/LocalIndustry/LocalIndustry/LI_DefOf.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace LocalIndustry
+{
+    [DefOf]
+    public static class LI_DefOf
+    {
+        public static PreceptDef LI_LocalIndustry;
+        public static ThoughtDef LI_CraftedItem;
+        public static ThoughtDef LI_CraftedBuilding;
+    }
+}
