52ff8effe1cf3b2d8a2540d2ccc23d4a4f2687f5
Taranchuk <nfhfyxer@gmail.com>
2023-01-29 09:40:26 +0200

    1.4 update

---
+++ 1.4/Source/LocalIndustry/LocalIndustry/GenRecipe_MakeRecipeProducts_Patch.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace LocalIndustry
+{
+    [HarmonyPatch(typeof(GenRecipe), "MakeRecipeProducts")]
+    public static class GenRecipe_MakeRecipeProducts_Patch
+    {
+        public static IEnumerable<Thing> Postfix(IEnumerable<Thing> __result, RecipeDef recipeDef, Pawn worker)
+        {
+            bool craftedItem = false;
+            foreach (var t in __result)
+            {
+                Log.Message("Worker: " + worker + " - " + worker.Faction);
+                if (worker.Faction == Faction.OfPlayer)
+                {
+                    craftedItem = true;
+                    GameComponent_ColonyItems.Instance.Add(t);
+                }
+                yield return t;
+            }
+
+            if (craftedItem && worker?.needs?.mood?.thoughts?.memories != null)
+            {
+                worker.needs.mood.thoughts.memories.TryGainMemory(LI_DefOf.LI_CraftedItem);
+            }
+        }
+    }
+}
---
+++ 1.4/Source/LocalIndustry/LocalIndustry/ScenPart_StartingThing_Defined_PlayerStartingThings_Patch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace LocalIndustry
+{
+    [HarmonyPatch(typeof(ScenPart_StartingThing_Defined), nameof(ScenPart_StartingThing_Defined.PlayerStartingThings))]
+    public static class ScenPart_StartingThing_Defined_PlayerStartingThings_Patch
+    {
+        public static IEnumerable<Thing> Postfix(IEnumerable<Thing> __result)
+        {
+            foreach (var t in __result)
+            {
+                GameComponent_ColonyItems.Instance.Add(t);
+                yield return t;
+            }
+        }
+    }
+}
